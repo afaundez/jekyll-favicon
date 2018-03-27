b5bf4ed8f2f1e99735256df05c5d2d57d0a42466
Alvaro Faundez <alvaro@faundez.net>
2018-03-26 22:26:39 -0400

    Update tests to new defaults

--- test/jekyll/favicon_test.rb
+++ test/jekyll/favicon_test.rb
@@ -5,11 +5,18 @@ describe Jekyll::Favicon do
     refute_nil Jekyll::Favicon::VERSION
   end
 
-  it 'it should a a config paramenter' do
+  it 'should have a config paramenter' do
     refute_empty Jekyll::Favicon.config
     refute_empty Jekyll::Favicon.config['source']
     refute_empty Jekyll::Favicon.config['path']
-    refute_empty Jekyll::Favicon.config['background']
-    refute_empty Jekyll::Favicon.config['sizes']
+    refute_empty Jekyll::Favicon.config['classic']
+    refute_empty Jekyll::Favicon.config['classic']['sizes']
+    refute_empty Jekyll::Favicon.config['apple-touch-icon']
+    refute_empty Jekyll::Favicon.config['apple-touch-icon']['background']
+    refute_empty Jekyll::Favicon.config['apple-touch-icon']['sizes']
+    refute_empty Jekyll::Favicon.config['ie']
+    refute_empty Jekyll::Favicon.config['ie']['sizes']
+    refute_empty Jekyll::Favicon.config['chrome']
+    refute_empty Jekyll::Favicon.config['chrome']['sizes']
   end
 end
