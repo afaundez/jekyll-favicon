0a0bd525a852b5c5b39c0b2f23a56272eaec6dc1
Alvaro Faundez <alvaro@faundez.net>
2019-11-14 17:40:38 -0500

    Reorder test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,21 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'jekyll-favicon'
 require 'minitest/autorun'
 require 'minitest/hooks/default'
 
+require 'jekyll-favicon'
 require 'jekyll'
+
 Jekyll.logger.log_level = :error
 
 def fixture(*subdirs)
   File.expand_path File.join('..', 'test', 'fixtures', *subdirs), __dir__
-end
-
-module Jekyll
-  module Favicon
-    class Asset
-      def inspect
-        "<#{self.class} name='#{@name}'>"
-      end
-    end
-  end
 end
