5993334836b26677836806ada1558a2ef1f8d529
Alvaro Faundez <alvaro@faundez.net>
2021-03-10 20:12:00 -0500

    refactor: config spec

---
+++ test/integration/default_config_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe 'favicon config' do
+  subject { Jekyll::Favicon.config }
+
+  it 'has default global source attribute' do
+    source_attribute = subject['source']
+    _(source_attribute).wont_be_nil
+    _(source_attribute).must_be_kind_of String
+  end
+
+  it 'has default global path attribute' do
+    path_attribute = subject['path']
+    _(path_attribute).wont_be_nil
+    _(path_attribute).must_be_kind_of String
+  end
+
+  describe 'targets' do
+    it 'has default chrome config' do
+      chrome_config = subject['chrome']
+      _(chrome_config).wont_be_nil
+      _(chrome_config).must_be_kind_of Hash
+      _(chrome_config['sizes']).must_be_kind_of Array
+    end
+
+    it 'has default classic config' do
+      classic_config = subject['classic']
+      _(classic_config).wont_be_nil
+      _(classic_config).must_be_kind_of Hash
+      _(classic_config['sizes']).must_be_kind_of Array
+    end
+
+    it 'has default ie config' do
+      ie_config = subject['ie']
+      _(ie_config).wont_be_nil
+      _(ie_config).must_be_kind_of Hash
+      _(ie_config['sizes']).must_be_kind_of Array
+    end
+
+    it 'has default safari config' do
+      safari_config = subject['apple-touch-icon']
+      _(safari_config).wont_be_nil
+      _(safari_config).must_be_kind_of Hash
+      _(safari_config['background']).must_be_kind_of String
+      _(safari_config['sizes']).must_be_kind_of Array
+    end
+  end
+end
