d5a20b39c53e91c2e2d678ed812eb6420eeaf2d3
Alvaro Faundez <alvaro@faundez.net>
2021-03-12 00:09:46 -0500

    refactor: crossorigin spec

---
+++ test/integration/manifest_crossorigin_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'nokogiri'
+
+describe 'when site defines chrome crossorigin value' do
+  context fixture: 'custom-config', process: true
+
+  let(:index_document) { Nokogiri::Slop File.open(index_destination) }
+  let(:index_destination) { File.join @destination, 'index.html' }
+
+  it 'should add crossorigin attribute to link tag' do
+    tag_attribute = Jekyll::Favicon.config.dig 'chrome', 'crossorigin'
+    tag_selector = %(link[crossorigin="#{tag_attribute}"])
+    _(index_document.at_css(tag_selector)).wont_be_nil
+  end
+end
