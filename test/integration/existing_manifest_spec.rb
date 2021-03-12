2ac5f2044e1634d8b14cfce10594d94c55a02540
Alvaro Faundez <alvaro@faundez.net>
2021-03-12 00:48:01 -0500

    refactor: existing manifest spec

---
+++ test/integration/existing_manifest_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'rexml/document'
+
+describe 'when site has an existing manifest at target location' do
+  context fixture: 'minimal-default-webmanifest', process: true
+
+  subject do
+    target_attribute = defaults.dig 'chrome', 'manifest', 'target'
+    webmanifest_path = @destination.join target_attribute
+    JSON.parse File.read(webmanifest_path)
+  end
+
+  it 'keeps values from existent webmanifest' do
+    _(subject).must_include 'name'
+  end
+
+  it 'appends icons webmanifest' do
+    _(subject).must_include 'icons'
+  end
+end
