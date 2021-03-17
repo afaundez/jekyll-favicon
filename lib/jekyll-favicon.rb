6595639d4f96b0f991e532e10206d50d201e3592
Alvaro Faundez <alvaro@faundez.net>
2021-03-17 18:47:52 -0400

    refactor: require_relatives not needed

--- lib/jekyll-favicon.rb
+++ lib/jekyll-favicon.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require 'jekyll'
-require_relative 'jekyll/favicon/utils'
-require_relative 'jekyll/favicon'
-require_relative 'jekyll/favicon/version'
-require_relative 'jekyll/favicon/hooks'
-require_relative 'jekyll/favicon/generator'
-require_relative 'jekyll/favicon/tag'
+require 'jekyll/favicon'
+require 'jekyll/favicon/version'
+require 'jekyll/favicon/utils'
+require 'jekyll/favicon/generator'
+require 'jekyll/favicon/tag'
+require 'jekyll/favicon/hooks'
