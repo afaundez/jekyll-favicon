70fbfa235af1092d5976d28349363e0b3aef250d
Alvaro Faundez <alvaro@faundez.net>
2021-03-18 21:05:26 -0400

    refactor: image into favicon utils

--- lib/jekyll/favicon/utils.rb
+++ lib/jekyll/favicon/utils.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
+require 'mini_magick'
+
 module Jekyll
   module Favicon
     # Favicon utils functions
     module Utils
       class << self
+        def convert(input, output, options = {})
+          MiniMagick::Tool::Convert.new do |convert|
+            options = convert_svg_options input, convert, options
+            convert << input
+            convert_options convert, options
+            convert << output
+          end
+        end
+
         def compact(compactable)
           case compactable
           when Hash, Array
@@ -31,6 +42,23 @@ module Jekyll
         end
 
         private
+
+        def convert_options(convert, options)
+          convert.flatten
+          options.each do |option, value|
+            convert.send option.to_sym, value
+          end
+        end
+
+        def convert_svg_options(input, convert, options)
+          return options unless File.extname(input) == '.svg'
+
+          return options unless (backgound = options.delete('background'))
+
+          backgound = 'none' if backgound == 'transparent'
+          convert.background backgound
+          options
+        end
 
         def deep_compact(compactable)
           case compactable
