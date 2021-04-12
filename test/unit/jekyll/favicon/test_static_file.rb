e5d04fb40beabf2896e0fb8e18ac07fca67dcbf1
Alvaro Faundez <alvaro@faundez.net>
2021-04-11 21:17:45 -0400

    Update ci (#33)
    * Add github action matrix testing with multiple jekyll
    
    * fix: mocks on 3.x tests
    
    * fix: rexml dependency
    
    * ci: exclude in matrix
    
    * ci: change exclude
    
    * ci: add jekyll 4.2 test and improve readme

--- test/unit/jekyll/favicon/test_static_file.rb
+++ test/unit/jekyll/favicon/test_static_file.rb
@@ -10,6 +10,13 @@ module Jekyll
       def setup
         @site = MiniTest::Mock.new
         @site.expect :source, '/dev/null'
+        frontmatter_defaults = Minitest::Mock.new
+        frontmatter_defaults.expect :all, {}, [Object, Object]
+        frontmatter_defaults.expect :all, {}, [Object, Object]
+        frontmatter_defaults.expect :all, {}, [Object, Object]
+        @site.expect :frontmatter_defaults, frontmatter_defaults
+        @site.expect :frontmatter_defaults, frontmatter_defaults
+        @site.expect :frontmatter_defaults, frontmatter_defaults
       end
 
       def test_raises_error_when_initializing_without_parameters
