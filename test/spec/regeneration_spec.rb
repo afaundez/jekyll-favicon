901dbe807625dd28ad1835dda93435551979c734
Alvaro Faundez <alvaro@faundez.net>
2021-05-25 20:29:50 +0000

    uncomment regenerator test case

--- test/spec/regeneration_spec.rb
+++ test/spec/regeneration_spec.rb
@@ -7,19 +7,19 @@ describe 'favicon regeneration' do
 
   subject { @context.destination.join 'favicon.png' }
 
-  # it 'will not modify files when reprocessing without changes' do
-  #   pre_mtime = File.mtime subject
-  #   @context.site.process
-  #   post_mtime = File.mtime subject
-  #   _(post_mtime).wont_be_nil
-  #   _(post_mtime).must_equal pre_mtime
-  # end
+  it 'will not modify files when reprocessing without changes' do
+    pre_mtime = File.mtime subject
+    @context.site.process
+    post_mtime = File.mtime subject
+    _(post_mtime).wont_be_nil
+    _(post_mtime).must_equal pre_mtime
+  end
 
   it 'will modify files when reprocessing after modifiying favicon source' do
     pre_mtime = File.mtime subject
 
     source_path = @context.source.join 'favicon.svg'
-    source_overrides = {'svg' => { 'circle' => { '_fill' => 'blue' } } }
+    source_overrides = { 'svg' => { 'circle' => { '_fill' => 'blue' } } }
     sleep 1 and File.write(source_path, build_site_favicon(source_overrides))
     @context.site.process
 
