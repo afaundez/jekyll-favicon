892104e97c559adee6061d33f7168af1a537befb
Alvaro Faundez <alvaro@faundez.net>
2021-06-10 16:57:40 +0000

    fix: disable jekyll-cache when running fixtures

--- test/fixtures/site.rb
+++ test/fixtures/site.rb
@@ -8,6 +8,7 @@ module Fixtures
   # Site fixture
   module Site
     def self.build(override)
+      override.merge! disable_disk_cache: false
       Fixtures::Jekyll.execute { return ::Jekyll::Site.new ::Jekyll.configuration override }
     end
 
